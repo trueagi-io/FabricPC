3c0000c111228f5d1a9494aca7bacb320ff60d58
charlie derr <charlie.derr@singularitynet.io>
2026-02-22 04:52:09 -0500

    Add comments for future development in identity.py
    Added initial comments for future testing.

--- fabricpc/nodes/identity.py
+++ fabricpc/nodes/identity.py
@@ -3,3 +3,8 @@
 IdentityNode just passes through its input as output, with no parameters and no error.
 This can be useful for things like input nodes or auxiliary nodes that don't need to learn anything, but just serve as conduits for data.
 """
+
+# Starting to work, on 2026.02.22 ~charlie
+"""
+First get a test coded up.
+"""
